081565dd175db51b63ef229b8a4945c8bff27ea0
weimuhua <weimuhua0812@126.com>
2016-03-14 11:20:50 +0800

    change : add prototype pattern.

--- src/me/com/patterns/Test.java
+++ src/me/com/patterns/Test.java
@@ -7,6 +7,10 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello IntelliJ Idea!");
 
+        testBuilder();
+    }
+
+    private static void testBuilder() {
         NutritionFacts nutritionFacts
                 = new NutritionFacts.Builder(5, 10)
                 .setCalories(3).setCarbohydrate(4)
---
+++ src/me/com/patterns/prototype/WordDocument.java
@@ -0,0 +1,43 @@
+package me.com.patterns.prototype;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wayne on 16-3-14.
+ * prototype pattern
+ * 原型模式，当类初始化需要消耗非常多的资源时，可通过使用原型拷贝来实现
+ */
+public class WordDocument implements Cloneable {
+
+    private String text;
+    private ArrayList<String> images = new ArrayList<>();
+
+    public WordDocument() {
+        System.out.printf("WordDocument construction");
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        WordDocument doc = (WordDocument) super.clone();
+        doc.text = this.text;
+        //doc.images = images;//浅拷贝
+        doc.images = new ArrayList<>(this.images);//深拷贝
+        return doc;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ArrayList<String> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<String> images) {
+        this.images = images;
+    }
+}
